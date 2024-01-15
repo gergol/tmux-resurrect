d9f3257487db7430232b4d1b4a13053f2958ba84
Gerald Golka <golka.gerald@gmail.com>
2024-01-15 11:35:00 +0100

    Added .session.vim as alternative session restore file

--- strategies/nvim_session.sh
+++ strategies/nvim_session.sh
@@ -8,7 +8,15 @@ ORIGINAL_COMMAND="$1"
 DIRECTORY="$2"
 
 nvim_session_file_exists() {
-	[ -e "${DIRECTORY}/Session.vim" ]
+    if [ -e "${DIRECTORY}/Session.vim" ]; then
+        echo "${DIRECTORY}/Session.vim"
+        return 0
+    elif [ -e "${DIRECTORY}/.session.vim" ]; then
+        echo "${DIRECTORY}/.session.vim"
+        return 0
+    else
+        return 1
+    fi
 }
 
 original_command_contains_session_flag() {
@@ -16,8 +24,8 @@ original_command_contains_session_flag() {
 }
 
 main() {
-	if nvim_session_file_exists; then
-		echo "nvim -S"
+	if SESSION_FILE=$(nvim_session_file_exists); then
+		echo "nvim -S $SESSION_FILE"
 	elif original_command_contains_session_flag; then
 		# Session file does not exist, yet the original nvim command contains
 		# session flag `-S`. This will cause an error, so we're falling back to
